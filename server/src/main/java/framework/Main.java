268d887b006dab6fd2feb7fcd0644b5526076395
Tair Sabirgaliev <tair.sabirgaliev@gmail.com>
2016-09-02 18:23:54 +0600

    Homework 1

--- server/src/main/java/framework/Main.java
+++ server/src/main/java/framework/Main.java
@@ -4,14 +4,24 @@ import io.undertow.Undertow;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
 
+import java.util.Deque;
+
 public class Main {
     public static void main(String[] args) {
         Undertow server = Undertow.builder()
                 .addHttpListener(8080, "localhost")
                 .setHandler((HttpServerExchange exchange) -> {
-                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/html");
 
-                    String response = "Empty";
+                    String response = "";
+
+                    Deque<String> aParams = exchange.getQueryParameters().get("a");
+                    Deque<String> bParams = exchange.getQueryParameters().get("b");
+
+                    if (aParams != null && !aParams.isEmpty()
+                            && bParams != null && !bParams.isEmpty()) {
+                        response = "a = " + aParams.getFirst() + ", b = " + bParams.getFirst();
+                    }
 
                     exchange.getResponseSender().send(response);
 
