63fa1f35a400bf22a97d11353413aafc2340c372
Tair Sabirgaliev <tair.sabirgaliev@gmail.com>
2016-09-02 14:42:52 +0600

    Initial commit

---
+++ server/src/main/java/framework/Main.java
@@ -0,0 +1,21 @@
+package framework;
+
+import io.undertow.Undertow;
+import io.undertow.server.HttpServerExchange;
+import io.undertow.util.Headers;
+
+public class Main {
+    public static void main(String[] args) {
+        Undertow server = Undertow.builder()
+                .addHttpListener(8080, "localhost")
+                .setHandler((HttpServerExchange exchange) -> {
+                    exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
+
+                    String response = "Empty";
+
+                    exchange.getResponseSender().send(response);
+
+                }).build();
+        server.start();
+    }
+}
