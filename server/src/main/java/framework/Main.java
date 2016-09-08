c30013f277f254e8dd2a0b763edca923a513342d
Tair Sabirgaliev <tair.sabirgaliev@gmail.com>
2016-09-08 17:02:55 +0600

    September 8 lab finished

--- server/src/main/java/framework/Main.java
+++ server/src/main/java/framework/Main.java
@@ -1,8 +1,20 @@
 package framework;
 
+import application.Bye;
+import application.Hello;
+import framework.api.Path;
+import framework.api.QueryParam;
 import io.undertow.Undertow;
 import io.undertow.server.HttpServerExchange;
 import io.undertow.util.Headers;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,7 +23,50 @@ public class Main {
                 .setHandler((HttpServerExchange exchange) -> {
                     exchange.getResponseHeaders().put(Headers.CONTENT_TYPE, "text/plain");
 
+                    try {
+                        Stream<Class<?>> classes = Stream.of(Hello.class, Bye.class);
+                        Stream<Method> methods = classes.flatMap(c -> Stream.of(c.getDeclaredMethods()));
 
+                        Stream<Method> methodsWithPath = methods.filter(m -> m.getAnnotation(Path.class) != null);
+
+                        Map<String, Method> methodByPath = methodsWithPath.collect(Collectors.toMap(
+                                m -> m.getAnnotation(Path.class).value(),
+                                m -> m
+                        ));
+
+                        Method method = methodByPath.get(exchange.getRequestPath());
+
+                        Stream<Parameter> paramsStream = Stream.of(method.getParameters());
+
+                        Object[] arguments = paramsStream.map(p -> {
+                            QueryParam annotation = p.getAnnotation(QueryParam.class);
+                            String paramName = annotation.value();
+                            String paramValue = exchange.getQueryParameters().get(paramName).getFirst();
+
+                            Class<?> paramType = p.getType();
+
+                            if (paramType.equals(Double.class)) {
+                                return Double.valueOf(paramValue);
+                            } else {
+                                return paramValue;
+                            }
+
+                        }).toArray();
+
+                        Object o = method.invoke(method.getDeclaringClass().newInstance(), arguments);
+
+                        String result = String.valueOf(o);
+
+                        exchange.getResponseSender().send(result);
+
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                        StringWriter writer = new StringWriter();
+                        e.printStackTrace(new PrintWriter(writer));
+                        String errorMessage = writer.toString();
+                        exchange.getResponseSender().send(errorMessage);
+                    }
 
                 }).build();
         server.start();
